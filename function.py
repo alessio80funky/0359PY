183bbdc081f5dfb33e56b8794bf91705e4de126c
alessio80funky <alessio80funky@gmail.com>
2026-08-12 12:15:14 +0900

    08/12 12:15

---
+++ function.py
@@ -0,0 +1,36 @@
+#関数
+
+def say_Hello():
+    print("こんにちは！")
+    print("Pythonは楽しい")
+#関数を呼び出す    
+say_Hello()
+
+
+def add(a,b):
+    return a + b
+
+result = add(10,5)
+
+print(result)
+
+######################################
+
+#変数代入する場合
+
+def greet(name):
+    return f"{name}さん、こんにちは！"
+
+name = greet("田中")
+
+print(name)
+
+#######################################
+
+#直接関数を実行する場合
+
+def greet(name):
+    return f"{name}さん、こんにちは！"
+
+print(greet("田中"))
+
--- variable.py
+++ variable.py
@@ -1,3 +1,5 @@
+#変数
+
 message = "Python メッセージ"
 age = 20
 
@@ -5,3 +7,23 @@ age = 20
 print(age)
 
 print(f"{message}と{age}")
+
+
+#変数に関しての注意：
+
+#変数に使えない変数名
+
+# 1 - 数字で始まる変数名　ー＞　123name = "message"
+# 2 -  アンダースコアで始まる変数名　ー＞　_name = "message"
+# 3 -  予約語　　下記となります：
+
+#False,class,is,none,continue,try,True,def,else,elif,for,if,whileなど
+
+False = 12
+
+print(False)
+
+  #File "C:\Users\kazuo\OneDrive\デスクトップ\python-3\variable.py", line 22
+    #False = 12
+   # ^^^^^
+#SyntaxError: cannot assign to False
