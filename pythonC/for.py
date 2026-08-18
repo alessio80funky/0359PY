3644aa0d108cb6b33e1831a95e238547ac0b2e45
gotokikakualessio-jpg <gotokikakualessio@gmail.com>
2026-08-18 11:15:32 +0900

    8/18 11:15

---
+++ pythonC/for.py
@@ -0,0 +1,16 @@
+for i in range(5):
+    print(i)
+
+
+fruits = ["りんご","バナナ","みかん"]
+
+for fruit in fruits:
+    print(fruit)
+
+count = 0
+
+while count < 5:
+    print(count)
+    count = count + 1 #whileを書くときに必ずこの記述を書きます
+
+print("繰り返し処理が終わりました。")
---
+++ pythonC/if.py
@@ -0,0 +1,27 @@
+score = 85
+
+if score >= 80:
+    print("合格です")
+else:
+    print("不合格です")
+
+#比較演算子と論理演算子を使ったif文
+
+value = 300.1
+
+if value >= 100 and value < 300:
+    print("範囲内")#True
+else: 
+    print("範囲外")#False
+
+a = True
+
+b = False
+
+print(a and b)#JS => &&　=> どちらも
+
+print(a or b)#JS => || =>　どれか
+
+print(not b)#JS => ! 否定演算子 
+
+#
